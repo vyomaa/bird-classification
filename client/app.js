7291fff4cdf5fedf68554f14c4032c0ecbe6e57a
vyomaa <vyoma2021@gmail.com>
2024-07-30 20:21:47 +0530

    Better UI
    Changed fonts for better readability

--- client/app.js
+++ client/app.js
@@ -1,36 +1,35 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     var form = document.getElementById('upload-form');
     var imageInput = document.getElementById('image-upload');
+    var uploadButton = document.getElementById('upload-button');
     var resultsDiv = document.getElementById('result');
     var uploadedImage = document.getElementById('uploaded-image');
     var label = document.getElementById('label');
     var scientificName = document.getElementById('scientific-name');
     var confidence = document.getElementById('confidence');
 
-    if (form) {
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
+    uploadButton.addEventListener('click', function() {
+        imageInput.click();
+    });
 
-            var file = imageInput.files[0];
-            var formData = new FormData();
-            formData.append('image', file);
+    imageInput.addEventListener('change', function() {
+        var file = imageInput.files[0];
+        var formData = new FormData();
+        formData.append('image', file);
 
-            fetch('http://127.0.0.1:5000/predict', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                uploadedImage.src = URL.createObjectURL(file);
-                uploadedImage.style.display = 'block';
+        fetch('http://127.0.0.1:5000/predict', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            uploadedImage.src = URL.createObjectURL(file);
+            uploadedImage.style.display = 'block';
 
-                label.innerText = 'Predicted Bird: ' + data.label;
-                scientificName.innerText = 'Scientific Name: ' + data.scientific_name;
-                //confidence.innerText = 'Confidence: ' + (data.confidence * 100).toFixed(2) + '%';
-            })
-            .catch(error => console.error('Error:', error));
-        });
-    } else {
-        console.error('Form element not found.');
-    }
+            label.innerText = data.label;
+            scientificName.innerText = 'Scientific Name: ' + data.scientific_name;
+            confidence.innerText = 'Confidence: ' + (data.confidence * 100).toFixed(2) + '%';
+        })
+        .catch(error => console.error('Error:', error));
+    });
 });
